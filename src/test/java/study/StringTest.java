3167fc96282f3da07e60bef5d1e36ed4747c920c
Hongvyo <wiswkd36@gmail.com>
2024-06-05 16:34:19 +0900

    testing!

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -8,6 +8,18 @@ public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
+        System.out.println("??");
+        assertThat(actual).isEqualTo("adc");
+    }
+    @Test
+    void splitByComma() {
+        assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void splitByCommaSingleElement() {
+        String actual = "abc".replace("b", "d");
+        System.out.println("??");
         assertThat(actual).isEqualTo("adc");
     }
 }
